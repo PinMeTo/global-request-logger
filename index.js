ed34c562f85807acc4273b448778bf05f5c20c0d
Morgan Persson <morgan+github@pinmeto.com>
2019-09-30 16:02:27 +0200

    Log https requests

--- index.js
+++ index.js
@@ -130,6 +130,7 @@ GlobalLog.prototype.initialize = function(options) {
   try {
     saveGlobals();
     http.request = attachLoggersToRequest.bind(http, 'http');
+    https.request = attachLoggersToRequest.bind(https, 'https');
     globalLogSingleton.isEnabled = true;
   } catch (e) {
     resetGlobals();
